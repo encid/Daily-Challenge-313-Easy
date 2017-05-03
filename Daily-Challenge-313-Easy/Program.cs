2a37aaf3593e8fb65071ff90cfc25ec66538ea01
encid <ryancav@gmail.com>
2017-05-03 07:09:16 -0400

    initial commit

--- Daily-Challenge-313-Easy/Program.cs
+++ Daily-Challenge-313-Easy/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DailyChallenge313
+namespace DailyChallenge313Easy
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(CheckSubsetSum(new List<int> { 1, 2, 3 }));
+            Console.WriteLine(CheckSubsetSum(new List<int> { -5, -3, -1, 2, 4, 6 }));
+            Console.WriteLine(CheckSubsetSum(new List<int> { }));
+            Console.WriteLine(CheckSubsetSum(new List<int> { -1, 1 }));
+            Console.WriteLine(CheckSubsetSum(new List<int> { -97364, -71561, -69336, 19675, 71561, 97863 }));
+            Console.WriteLine(CheckSubsetSum(new List<int> { -53974, -39140, -36561, -23935, -15680, 0 }));
+
+            Console.ReadLine();
+        }
+
+        static bool CheckSubsetSum(List<int> input)
+        {
+            if (input.Count == 0) return false;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] == 0) return true;
+                if (input.Contains(input[i] * -1)) return true;                
+            }
+
+            return false;
         }
     }
 }
--- Daily-Challenge-313-Easy/Properties/AssemblyInfo.cs
+++ Daily-Challenge-313-Easy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Daily-Challenge-313")]
+[assembly: AssemblyTitle("Daily-Challenge-313-Easy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Daily-Challenge-313")]
+[assembly: AssemblyProduct("Daily-Challenge-313-Easy")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
